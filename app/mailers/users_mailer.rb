2074798ad96b35bc0f4ac9ce818fabaaa4ed5125
vvs79 <valentyn-vvs@rambler.ru>
2016-06-02 17:59:45 +0300

    lunch

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,11 +1,11 @@
 class UsersMailer < ApplicationMailer
 	def order_destroyed(order)
     @order = order
-    mail to: 'vidoc1979@gmail.com', subject: "User: #{@order.user.email} destroyed order: #{@order.id}"
+    mail to: 'vvs.lunch@gmail.com', subject: "User: #{@order.user.email} destroyed order: #{@order.id}"
   end
 
   def order_created(order)
     @order = order
-    mail to: 'vidoc1979@gmail.com', subject: "User: #{@order.user.email} created order: #{@order.id}"
+    mail to: 'vvs.lunch@gmail.com', subject: "User: #{@order.user.email} created order: #{@order.id}"
   end
 end
